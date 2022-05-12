ff6d0dc2a34e743776e3b213b0fc9757dfb6783d
sameersheikh2 <sameersheikh819@gmail.com>
2022-05-12 10:26:35 +0530

    project started

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <fragment>
+      <header></header>
+      <div></div>;
+    </fragment>
+  );
+};
+
+export default Header;
